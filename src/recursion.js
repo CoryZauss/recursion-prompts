bf6d0d1b90ed7567cf7bb4288169e26978454688
Cory Zauss <cory.zauss@gmail.com>
2021-10-20 15:20:02 -0400

    complete 16

--- src/recursion.js
+++ src/recursion.js
@@ -349,6 +349,16 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  var arr = [];
+  if (str.length === 0) {
+    return arr;
+  }
+  arr.push(str[0])
+
+
+  var newStr = str.slice(1)
+  return arr.concat(createArray(newStr))
+
 };
 
 // 17. Reverse the order of an array
